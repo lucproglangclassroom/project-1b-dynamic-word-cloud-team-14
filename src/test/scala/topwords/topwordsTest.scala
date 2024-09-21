9e50996604ba6b0c865d02a2891247b544c9473f
bortiz-101 <bortiz101@hotmail.com>
2024-09-20 22:06:19 -0500

    initial topwordsTest

---
+++ src/test/scala/topwords/topwordsTest.scala
@@ -0,0 +1,63 @@
+package topwords
+
+import scala.collection.mutable.Stack
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.Suite
+import org.scalatest.matchers.must.Matchers.*
+
+import scala.language.postfixOps
+
+class topwordsTest extends AnyFlatSpec with Suite:
+  
+  test("iterator should correctly iterate through lines in testFile.txt and store as an array") {
+
+    val filePath = Paths.get("doc", "lestimes10-1.txt").toFile 
+    val source = Source.fromFile(filePath)
+    val inputStream = new ByteArrayInputStream(source.getBytes)
+    
+    val iteratorClass = new IteratorClass(inputStream)
+    val result = iteratorClass.getLines
+    
+    //Assert
+    assert(result.sameElements(source.toArray))
+
+
+    source.close()
+  }
+  test("CounterClass should correctly count occurrences of each unique word in the array") {
+
+    val words = Array("apple", "banana", "apple", "orange", "banana", "apple")
+    
+    val counter = new CounterClass(words)
+    val result = counter.getWordCount
+
+    // Assert
+    assert(result == Map("apple" -> 3, "banana" -> 2, "orange" -> 1))
+  }
+
+  test("CounterClass should handle an empty array") {
+
+    val emptyArray = Array.empty[String]
+
+
+    val counter = new CounterClass(emptyArray)
+    val result = counter.getWordCount
+
+    // Assert
+    assert(result.isEmpty)
+  }
+
+  test("CounterClass should handle an array with one word") {
+
+    val singleWordArray = Array("apple")
+
+
+    val counter = new CounterClass(singleWordArray)
+    val result = counter.getWordCount
+
+    // Assert
+    assert(result == Map("apple" -> 1))
+  }
+
+  
+end topwordsTest
